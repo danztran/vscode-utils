9ac4f15d4bb8f62ba22d6530734f26ab4a261f00
danztran <nguyenbao1917@gmail.com>
2024-08-09 15:45:33 +0700

    feat!: replace with pnpm, add folder selection

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,41 @@
-import * as vscode from "vscode";
-import { ExtensionContext } from "vscode";
-import { DefaultLogger } from "./logger";
+import * as vscode from 'vscode';
+import { ExtensionContext } from 'vscode';
+import { DefaultLogger } from './logger';
 
 export class App extends DefaultLogger {
   private ctx: ExtensionContext;
   constructor(ctx: ExtensionContext) {
-    const channel = vscode.window.createOutputChannel("Utils");
+    const channel = vscode.window.createOutputChannel('Utils');
     super({
-      logLevel: "debug",
+      logLevel: 'debug',
       channel,
     });
     this.ctx = ctx;
-    this.removeOtherFolders = this.removeOtherFolders.bind(this);
+    this.removeFoldersByCurrentFile =
+      this.removeFoldersByCurrentFile.bind(this);
   }
 
-  removeOtherFolders(): boolean {
+  async removeOtherFolders(): Promise<boolean> {
+    // this script prompt use to select a folder to keep
+    const { workspace } = vscode;
+    const folders = workspace.workspaceFolders;
+    if (!folders) {
+      return false;
+    }
+    const folderNames = folders.map(folder => folder.name);
+    const folderName = await vscode.window.showQuickPick(folderNames);
+    if (!folderName) {
+      return false;
+    }
+    const folder = folders.find(folder => folder.name === folderName);
+    if (!folder) {
+      return false;
+    }
+    workspace.updateWorkspaceFolders(0, folders.length, folder);
+    return true;
+  }
+
+  removeFoldersByCurrentFile(): boolean {
     const folder = this.getActiveWorkspaceFolder();
     if (!folder) {
       return false;
@@ -33,7 +54,7 @@ export class App extends DefaultLogger {
       return false;
     }
     const fileName = editor.document.fileName;
-    for (const folder of folders) {
+    for (const folder of [...folders].reverse()) {
       const path = folder.uri.path;
       if (fileName.includes(path)) {
         return folder;
--- src/extension.ts
+++ src/extension.ts
@@ -1,19 +1,27 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from "vscode";
-import { App } from "./app";
+import * as vscode from 'vscode';
+import { App } from './app';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(ctx: vscode.ExtensionContext) {
   const app = new App(ctx);
 
-  const removeOtherFoldersInWorkspace = vscode.commands.registerCommand(
-    "vscode-utils.removeOtherFoldersFromWorkspace",
+  const removeOtherFoldersInCurrentWorkspace = vscode.commands.registerCommand(
+    'vscode-utils.removeOtherFoldersFromWorkspaceByCurrentFile',
+    app.removeFoldersByCurrentFile,
+  );
+
+  const removeOtherFoldersFromWorkspace = vscode.commands.registerCommand(
+    'vscode-utils.removeOtherFoldersFromWorkspace',
     app.removeOtherFolders,
   );
 
-  ctx.subscriptions.push(removeOtherFoldersInWorkspace);
+  ctx.subscriptions.push(
+    removeOtherFoldersInCurrentWorkspace,
+    removeOtherFoldersFromWorkspace,
+  );
 }
 
 // this method is called when your extension is deactivated
